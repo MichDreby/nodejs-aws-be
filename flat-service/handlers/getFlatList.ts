7445acc5a9319f1e6095ca5e72de0ed3840315f1
michdreby <mr.sincee@gmail.com>
2020-11-15 20:48:00 +0300

    feat:lambdas/connect getFlatList + getFlatById to database

--- flat-service/handlers/getFlatList.ts
+++ flat-service/handlers/getFlatList.ts
@@ -1,23 +1,62 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import flats from '../mocks/flats.json';
+import  { Client } from 'pg';
+
+
+const {
+  PG_HOST,
+  PG_PORT,
+  PG_DATABASE,
+  PG_USERNAME,
+  PG_PASSWORD
+} = process.env;
+
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+}
 
 
 export const getFlatList: APIGatewayProxyHandler = async (event, _context) => {
+  const client = new Client(dbOptions);
+
   try {
     console.log(
       '**********\n',
       'event',
       event
     );
+  
+    await client.connect();
+  
+    const { rows } = await client.query(`
+      SELECT id, address, area, district, price, rooms, count
+      FROM flats
+      inner join stocks
+      ON flats.id = stocks.flat_id    
+    `);
+
+    console.log(
+      '**********\n',
+      'rows',
+      rows
+    );
     
     return {
       statusCode: 200,
-      body: JSON.stringify(flats),
+      body: JSON.stringify(rows),
       headers: {
         'Access-Control-Allow-Origin': '*'
       }
     };
+
   } catch(error) {
     console.log(
       '**********\n',
@@ -26,7 +65,7 @@ export const getFlatList: APIGatewayProxyHandler = async (event, _context) => {
     );
 
     return {
-      statusCode: 405,
+      statusCode: 500,
       body: JSON.stringify({
         message: 'Something went wrong, please try again later'
       }),
@@ -34,5 +73,9 @@ export const getFlatList: APIGatewayProxyHandler = async (event, _context) => {
         'Access-Control-Allow-Origin': '*'
       }
     }
-  }  
+  }
+  finally {
+    await client.end();
+  }
+
 }
